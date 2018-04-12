9c2e196319226cfdcf178ccb9d853b2c01fb2425
YanaRyssjanova <night-vamp@rumbler.ru>
2018-04-12 20:15:33 +0300

    стили work

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
 
+let bodyParser = require("body-parser");
+let mysql=require("mysql");
+let $=require("jquery/dist/jquery.js");
 
+let index = require('./routes/index');
+let users = require('./routes/users');
 
-
-
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-
-var bodyParser = require("body-parser");
-var mysql=require("mysql");
-var $=require("jquery/dist/jquery.js");
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,15 +20,18 @@ app.set('view engine', 'ejs');
 
 
 // создаем парсер для данных application/x-www-form-urlencoded
-var urlencodedParser = bodyParser.urlencoded({extended: false});
+let urlencodedParser = bodyParser.urlencoded({extended: false});
 
 
+app.use(express.static(__dirname+'/views'));
+// Обьединение
 
-//var st;!!!!!!!!!!!!!!!
+
+//let st;!!!!!!!!!!!!!!!
 app.post("/form_handler", urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
-    var b=request.body.first_name;
+    let b=request.body.first_name;
     console.log("b "+b);
 
     //st=b;!!!!!!!!!
@@ -41,7 +40,7 @@ app.post("/form_handler", urlencodedParser, function (request, response) {
 
 });
 
-var connection=mysql.createConnection({
+let connection=mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'1234',
@@ -51,8 +50,8 @@ var connection=mysql.createConnection({
 
 
 
-var a;
-var query=connection.query("Select * from lab5 where id > 30",function (error,fields,result) {
+let a;
+let query=connection.query("Select * from lab5 where id > 30",function (error,fields,result) {
     if (error) throw error;
      a=fields[4].Name;
     console.log(a);
@@ -60,9 +59,9 @@ var query=connection.query("Select * from lab5 where id > 30",function (error,fi
 
 
 app.get("/index1", function(request, response){
-var a1=0;
-var c;
-    var query=connection.query("Select * from lab5 where id > 30",function (error,fields,result) {
+let a1=0;
+let c;
+    let query=connection.query("Select * from lab5 where id > 30",function (error,fields,result) {
 // title:  st,!!!!!!!!!!!!!!!!!!
         if (error) throw error;
         console.log(fields[0].Name);
@@ -73,9 +72,10 @@ for(a1=1;a1<5;a1++){
        c=c.substr(0, c.length - 1);
         console.log(c);
         a1=c.split(",");
-        for(var i=0;i<a1.length;i++){
+        for(let i=0;i<a1.length;i++){
             console.log("a1 "+a1[i]);
         }
+        //response.sendFile(path.join('/'));
         response.render("index1", {
             optionl:a1
         });
@@ -96,7 +96,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
